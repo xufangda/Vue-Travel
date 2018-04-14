299769b881a1ec23c93469d3a2bd6a21594e343d
fangda <poi998998@gmail.com>
2018-04-14 23:15:06 +0100

    recommand added

---
+++ frontend/src/pages/home/components/Recommand.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommandList" :key='item.id'>
+        <img class="item-img" :src='item.imgUrl'>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}}</p>
+          <p class="item-desc">{{item.desc}}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommand',
+  data () {
+    return {
+      recommandList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/7b/7b023b9728f0834fa3.img.jpg_200x200_5fddc1d8.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/7b/7b023b9728f0834fa3.img.jpg_200x200_5fddc1d8.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1803/7b/7b023b9728f0834fa3.img.jpg_200x200_5fddc1d8.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫的大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+  .title
+    margin-top:.2rem
+    line-height: .8rem
+    background : #eee
+    text-indent:.2rem
+  .item
+    display :flex
+    height: 1.9rem
+    overflow : hidden
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex:1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color:#ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        background: #ff9300
+        padding: 0 .2rem
+        margin-top: .16rem
+        border-radius: .06rem
+        color: #fff
+</style>
--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommand></home-recommand>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommand from './components/Recommand'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommand
   }
 }
 </script>
