f5daea62157d44ed70c7458e89b1d12599f35aeb
fangda <poi998998@gmail.com>
2018-04-14 18:04:15 +0100

    change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,12 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick' // 在使用click事件时，某些移动端会有300ms延迟，通过引入库可以解决这个问题
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 import 'styles/reset.css' // 统一各浏览器表现的基本css
 import 'styles/border.css' // 解决高倍评一像素边框的问题
 import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ frontend/src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+           <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1802/73/d1ba8edc561d1f02.jpg_640x200_a747973f.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/81/61e16fc667c0bb02.jpg_640x200_ea28d62a.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // 三箭头表示穿透，不受scoped控制，可以直接穿透组件控制样式
+  .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width:100%
+</style>
