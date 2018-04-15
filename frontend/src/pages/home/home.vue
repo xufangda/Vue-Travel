c008e979fa1dbf198ad9394b10d3376c4a5e5105
fangda <poi998998@gmail.com>
2018-04-16 00:12:53 +0100

    index-weekend

--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -4,6 +4,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommand></home-recommand>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommand from './components/Recommand'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HomeRecommand
+    HomeRecommand,
+    HomeWeekend
   }
 }
 </script>
