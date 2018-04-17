9a8416fb6c3b48465fe92af01cacfe36e3aff9fe
fangda <poi998998@gmail.com>
2018-04-17 23:48:30 +0100

    finish css

---
+++ frontend/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'city'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    position  absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .54rem
+    .item
+      line-height .44rem
+      text-align center
+      color $bgColor
+</style>
