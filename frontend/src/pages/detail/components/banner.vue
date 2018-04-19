4f1d68f225de5515fa7d4b3d43ffd3a3a018b083
fangda <poi998998@gmail.com>
2018-04-19 23:43:29 +0100

    banner image

---
+++ frontend/src/pages/detail/components/banner.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="banner">
+    <img
+    src="http://img1.qunarzz.com/sight/p0/201403/10/b892e5069c3508f7b017a91121502223.jpg_600x330_b421353e.jpg"
+    class="banner-img">
+    <div class="banner-info">
+      <div class="banner-title">
+        古北水镇
+      </div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe692;</span>39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    overflow hidden
+    height 0
+    padding-bottom  55%
+    position relative
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color white
+      background-image linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
+      .banner-title
+        font-size .32rem
+        padding 0 .2rem
+        flex 1
+      .banner-number
+        margin-top .14rem
+        padding  0 .4rem
+        height .32rem
+        line-height .32rem
+        border-radius .2rem
+        background rgba(0,0,0,.8)
+        font-size .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
