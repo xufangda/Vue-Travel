fb93b7f09de8e35192679f5a9965d89bd5db956e
fangda <poi998998@gmail.com>
2018-04-22 00:42:03 +0100

    recursive component

--- frontend/src/pages/detail/detail.vue
+++ frontend/src/pages/detail/detail.vue
@@ -2,19 +2,44 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content">11223</div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
 
 export default {
   name: 'detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三管联票',
+          children: [{
+            title: '成人三管联票 - 某一连锁店销售'
+          }]
+        }, {
+          title: '成人五管联票'
+        }]
+      }, {
+        title: '成人票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
