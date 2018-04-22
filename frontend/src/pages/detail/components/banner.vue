d60a0c1df0ba8b27b7b9f730dfa714a387042081
fangda <poi998998@gmail.com>
2018-04-22 01:14:03 +0100

    detail-ajax

--- frontend/src/pages/detail/components/banner.vue
+++ frontend/src/pages/detail/components/banner.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
     <div class="banner" @click="handleBannerClick">
-      <img
-      src="http://img1.qunarzz.com/sight/p0/201403/10/b892e5069c3508f7b017a91121502223.jpg_600x330_b421353e.jpg"
-      class="banner-img">
+      <img :src="bannerImg" class="banner-img">
       <div class="banner-info">
         <div class="banner-title">
-          古北水镇
+          {{sightName}}
         </div>
         <div class="banner-number">
-          <span class="iconfont banner-icon">&#xe692;</span>39
+          <span class="iconfont banner-icon">&#xe692;</span> {{gallaryImgs.length}}
         </div>
       </div>
     </div>
-    <common-gallery :imgs="imgs" @close="handleGallaryClose" v-show="showGallery"></common-gallery>
+    <common-gallery :imgs="gallaryImgs" @close="handleGallaryClose" v-show="showGallery"></common-gallery>
   </div>
 </template>
 
@@ -21,11 +19,14 @@
 import CommonGallery from 'common/gallary/Gallary'
 export default {
   name: 'detailBanner',
+  props: {
+    sightName: String,
+    bannerImg: String,
+    gallaryImgs: Array
+  },
   data () {
     return {
-      showGallery: false,
-      imgs: [
-        'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_r_800x800_e9c1d2be.jpg']
+      showGallery: false
     }
   },
   components: {
--- frontend/src/pages/detail/detail.vue
+++ frontend/src/pages/detail/detail.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <detail-banner></detail-banner>
+    <detail-banner
+      :sightName="sightName"
+      :bannerImg="bannerImg"
+      :gallaryImgs="gallaryImgs"></detail-banner>
     <detail-header></detail-header>
     <div class="content">
       <detail-list :list="list"></detail-list>
@@ -12,6 +15,7 @@
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
 import DetailList from './components/list'
+import axios from 'axios'
 
 export default {
   name: 'detail',
@@ -22,24 +26,34 @@ export default {
   },
   data () {
     return {
-      list: [{
-        title: '成人票',
-        children: [{
-          title: '成人三管联票',
-          children: [{
-            title: '成人三管联票 - 某一连锁店销售'
-          }]
-        }, {
-          title: '成人五管联票'
-        }]
-      }, {
-        title: '成人票'
-      }, {
-        title: '儿童票'
-      }, {
-        title: '特惠票'
-      }]
+      sightName: '',
+      bannerImg: '',
+      gallaryImgs: [],
+      list: []
     }
+  },
+  methods: {
+    getDetailInfo () {
+      // axios.get('/api/detail.json?id=' + this.$route.params.id)
+      axios.get('/api/detail.json', {
+        params: {
+          id: this.$route.params.id
+        }
+      }).then(this.handleGtDataSucc)
+    },
+    handleGtDataSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.sightName = data.sightName
+        this.bannerImg = data.bannerImg
+        this.gallaryImgs = data.gallaryImgs
+        this.list = data.categoryList
+      }
+    }
+  },
+  mounted () {
+    this.getDetailInfo()
   }
 }
 </script>
